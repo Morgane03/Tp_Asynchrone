dc3d4a989202163a98bbf4646c46f3be090542ed
m v <morgane.voisin@e-artsup.net>
2024-01-25 12:15:54 +0100

    Fin exo1

---
+++ Assets/Script/Exo1/CubeTurn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CubeTurn : MonoBehaviour
+{
+    [SerializeField] private GameObject _cube;
+
+    private IEnumerator TurnCube()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime <= 5f)
+        {
+            _cube.transform.Rotate(0, 45 * Time.deltaTime, 0);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    public void TurnCubeOnClick()
+    {
+        StartCoroutine(TurnCube());
+    }
+
+    public void StopCubeOnClick()
+    {
+        StopCoroutine(TurnCube());
+    }
+}
